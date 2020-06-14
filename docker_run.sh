ef6d37c0d5043cdbe33f11c4d260ebda9aa875a0
Alex Kremer <godexsoft@gmail.com>
2020-06-14 16:11:11 +0100

    Update docker test script

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -d --rm -v "$(pwd)"/examples:/data:rw -p 8080:8080 --name krabby godexsoft/krabby:0.0.3
+docker run -d --rm -v "$(pwd)"/examples:/data:rw -p 8080:8080 --name krabby godexsoft/krabby:0.0.4
